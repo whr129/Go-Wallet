5d84b2ec283b3622d2ed52d71455fac494cba6e7
whr129 <howardwang0315@gmail.com>
2025-07-29 22:27:05 -0400

    fix: dependency

--- pkg/util/get_header.go
+++ pkg/util/get_header.go
@@ -13,17 +13,21 @@ const (
 
 func GetXUserID(ctx *gin.Context) (int64, bool) {
 	xUserID := ctx.GetHeader(X_USER_ID)
+
 	userID, err := strconv.ParseInt(xUserID, 10, 64)
 	if err != nil {
 		return 0, false
 	}
+
 	return userID, true
 }
 
 func GetXEmail(ctx *gin.Context) (string, bool) {
 	xEmail := ctx.GetHeader(X_EMAIL)
+
 	if xEmail == "" {
 		return "", false
 	}
+
 	return xEmail, true
 }
